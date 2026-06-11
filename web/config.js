c84c2cf33de2f736f75f5aba5dd768c5f48d81a4
Thirdchai Sattayapanich <thirdchaisattayapanich@Thirdchais-MacBook-Air.local>
2026-06-11 15:50:01 +0700

    feat: set LIFF ID

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 window.DORM_ORDER_CONFIG = {
   supabaseUrl: "https://gpoybeeuztxrygaeqbdq.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwb3liZWV1enR4cnlnYWVxYmRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3MjkwNDQsImV4cCI6MjA5NjMwNTA0NH0.WKBQrsSqch7K2R9C9NP1Ece8TViIYT_zmaykU3-i9L8",
-  liffId: "",
+  liffId: "2010369827-0eYUVJh5",
   shopName: "กะเพราไฟลุก by MEAL TEAory",
 };
